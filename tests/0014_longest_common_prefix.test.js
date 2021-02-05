8e9a0ab9710fe03ce0c017a0e9c255cf4fcccdfe
abiaqiou <abiaqiou@gmail.com>
2021-02-05 13:27:17 +0800

    add problem 0014

---
+++ tests/0014_longest_common_prefix.test.js
@@ -0,0 +1,21 @@
+const longestCommonPrefix = require('../problems/0014_longest_common_prefix')
+
+test('0014 longestCommonPrefix test 01', () => {
+    const result = longestCommonPrefix(["flower", "flow", "flight"])
+    expect(result).toBe('fl')
+})
+
+test('0014 longestCommonPrefix test 02', () => {
+    const result = longestCommonPrefix(["dog", "racecar", "car"])
+    expect(result).toBe('')
+})
+
+test('0014 longestCommonPrefix test 03', () => {
+    const result = longestCommonPrefix([])
+    expect(result).toBe('')
+})
+
+test('0014 longestCommonPrefix test 04', () => {
+    const result = longestCommonPrefix(["dog"])
+    expect(result).toBe('dog')
+})
