af385d8b5f9316ad2e4448a13714e0cf97ac3471
abiaqiou <abiaqiou@gmail.com>
2021-03-11 19:23:30 +0800

    add problem 0028

---
+++ tests/0028_implement_strstr.test.js
@@ -0,0 +1,11 @@
+const strStr = require('../problems/0028_implement_strstr')
+
+test('0028 strStr test 01', () => {
+  const result = strStr('hello', 'll')
+  expect(result).toBe(2)
+})
+
+test('0028 strStr test 02', () => {
+  const result = strStr('aaaaa', 'bba')
+  expect(result).toBe(-1)
+})
