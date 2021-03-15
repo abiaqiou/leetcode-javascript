b24832a10f8dbca6b32f35c9cb617d7ad18796fe
abiaqiou <abiaqiou@gmail.com>
2021-03-15 11:38:44 +0800

    add problem 0058

---
+++ tests/0058_length_of_last_word.test.js
@@ -0,0 +1,13 @@
+const lengthOfLastWord = require('../problems/0058_length_of_last_word')
+
+test('0058 lengthOfLastWord test 01', () => {
+  const s = 'Hello World'
+  const result = lengthOfLastWord(s)
+  expect(result).toBe(5)
+})
+
+test('0058 lengthOfLastWord test 02', () => {
+  const s = ' '
+  const result = lengthOfLastWord(s)
+  expect(result).toBe(0)
+})
