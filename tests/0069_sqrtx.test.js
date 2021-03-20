47c834f74dba86877494dac73efac9c5601f6da0
abiaqiou <abiaqiou@gmail.com>
2021-03-20 11:11:47 +0800

    add problem 0069

---
+++ tests/0069_sqrtx.test.js
@@ -0,0 +1,16 @@
+const mySqrt = require('../problems/0069_sqrtx')
+
+test('0069 mySqrt test 01', () => {
+  const result = mySqrt(4)
+  expect(result).toBe(2)
+})
+
+test('0069 mySqrt test 02', () => {
+  const result = mySqrt(8)
+  expect(result).toBe(2)
+})
+
+test('0069 mySqrt test 03', () => {
+  const result = mySqrt(2501)
+  expect(result).toBe(50)
+})
