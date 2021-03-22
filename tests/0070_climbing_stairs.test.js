267f5dc00a1189111d6009cca68c04fcaabed144
abiaqiou <abiaqiou@gmail.com>
2021-03-22 11:17:57 +0800

    add problem 0070

---
+++ tests/0070_climbing_stairs.test.js
@@ -0,0 +1,16 @@
+const climbStairs = require('../problems/0070_climbing_stairs')
+
+test('0070 climbStairs test 01', () => {
+  const result = climbStairs(1)
+  expect(result).toBe(1)
+})
+
+test('0070 climbStairs test 01', () => {
+  const result = climbStairs(2)
+  expect(result).toBe(2)
+})
+
+test('0070 climbStairs test 01', () => {
+  const result = climbStairs(3)
+  expect(result).toBe(3)
+})
