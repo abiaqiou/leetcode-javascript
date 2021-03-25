a403d4b32dbd4228910482964917d0836d071e6a
abiaqiou <abiaqiou@gmail.com>
2021-03-25 10:22:15 +0800

    add problem 0118

---
+++ tests/0118_pascals_triangle.test.js
@@ -0,0 +1,22 @@
+const generate = require('../problems/0118_pascals_triangle')
+
+test('0118 generate test 01', () => {
+  const result = generate(1)
+  expect(result).toEqual([[1]])
+})
+
+test('0118 generate test 02', () => {
+  const result = generate(2)
+  expect(result).toEqual([[1], [1, 1]])
+})
+
+test('0118 generate test 03', () => {
+  const result = generate(5)
+  expect(result).toEqual([
+    [1],
+    [1, 1],
+    [1, 2, 1],
+    [1, 3, 3, 1],
+    [1, 4, 6, 4, 1],
+  ])
+})
